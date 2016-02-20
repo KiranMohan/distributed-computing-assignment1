b72d5d5fdc646224162381d11e692955b7de7beb
Kiran Mohan <kiranmohan@123.com>
2016-02-20 23:04:16 +0530

    formatting

--- client-user-service/src/clientUserService.cpp
+++ client-user-service/src/clientUserService.cpp
@@ -13,78 +13,77 @@
                             from lib-user-service/user-service.x*/
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     CLIENT *clnt;
     int *result;
     char *server;
     char *message;
 
     if (argc != 3) {
-        fprintf(stderr, "usage: %s host message\n", argv[0]);
-                exit(1);
-            }
+        fprintf(stderr, "usage: %s host\n", argv[0]);
+        exit(1);
+    }
 
-            server = argv[1];
-            message = argv[2];
+    server = argv[1];
+    message = argv[2];
 
-            /*
-             * Create client "handle" used for
-             * calling MESSAGEPROG on the server
-             * designated on the command line.
-             */
+    /*
+     * Create client "handle" used for
+     * calling MESSAGEPROG on the server
+     * designated on the command line.
+     */
 
-            clnt = clnt_create(server, MESSAGEPROG, PRINTMESSAGEVERS, "tcp");
+    clnt = clnt_create(server, MESSAGEPROG, PRINTMESSAGEVERS, "tcp");
 
-            if (clnt == (CLIENT *)NULL) {
-                /*
-                 * Couldn't establish connection
-                 * with server.
-                 * Print error message and die.
-                 */
+    if (clnt == (CLIENT *) NULL) {
+        /*
+         * Couldn't establish connection
+         * with server.
+         * Print error message and die.
+         */
 
-                clnt_pcreateerror(server);
-                exit(1);
-            }
+        clnt_pcreateerror(server);
+        exit(1);
+    }
 
-            /*
-             * Call the remote procedure
-             * "printmessage" on the server
-             */
+    /*
+     * Call the remote procedure
+     * "printmessage" on the server
+     */
 
-            result = printmessage_1(&message, clnt);
-            if (result == (int *)NULL) {
-                /*
-                 * An error occurred while calling
-                 * the server.
-                 * Print error message and die.
-                 */
+    result = printmessage_1(&message, clnt);
+    if (result == (int *) NULL) {
+        /*
+         * An error occurred while calling
+         * the server.
+         * Print error message and die.
+         */
 
-                clnt_perror(clnt, server);
-                exit(1);
-            }
+        clnt_perror(clnt, server);
+        exit(1);
+    }
 
-            /* Okay, we successfully called
-             * the remote procedure.
-             */
+    /* Okay, we successfully called
+     * the remote procedure.
+     */
 
-            if (*result == 0) {
+    if (*result == 0) {
 
-                /*
-                 * Server was unable to print
-                 * our message.
-                 * Print error message and die.
-                 */
+        /*
+         * Server was unable to print
+         * our message.
+         * Print error message and die.
+         */
 
-                fprintf(stderr, "%s: could not print your message\n",argv[0]);
-                exit(1);
-            }
+        fprintf(stderr, "%s: could not print your message\n", argv[0]);
+        exit(1);
+    }
 
-            /* The message got printed on the
-             * server's console
-             */
+    /* The message got printed on the
+     * server's console
+     */
 
-            printf("Message delivered to %s\n", server);
-            clnt_destroy( clnt );
-            exit(0);
-        }
+    printf("Message delivered to %s\n", server);
+    clnt_destroy(clnt);
+    exit(0);
+}
